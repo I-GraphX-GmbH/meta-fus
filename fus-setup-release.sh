d653075744e5ddef3b521e755017a59fc7b639c7
Philipp Gerbach <gerbach@fs-net.de>
2020-02-06 13:18:36 +0000

    Add fus-image-mono
    The fus mono image should help customers to check whether their
    .NET-framework application is portable to Linux or not.
    It has the open-source framework Mono preinstalled so the user can try
    to run their .NET application and see if all the necessary libraries are
    available.
    
    It is only available for x11 for now because of the dependency of
    libgdiplus.

--- fus-setup-release.sh
+++ fus-setup-release.sh
@@ -48,6 +48,7 @@ DISTRO=$DISTRO MACHINE=$MACHINE . $FSL_SETUP_RELEASE -b $BUILD_DIR
 
 # Add FuS-Layer
 echo "" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \" \${BSPDIR}/sources/meta-mono \"" >> $BUILD_DIR/conf/bblayers.conf
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-fus \"" >> $BUILD_DIR/conf/bblayers.conf
 
 # Determine root file system mode
